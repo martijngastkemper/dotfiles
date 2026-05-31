68df1c8963a7c5ec84845557aab3ed309f7c5afa
Martijn Gastkemper <contact@martijngastkemper.nl>
2026-05-31 12:37:21 +0200

    Fix editorconfig errors

--- fixup.zsh
+++ fixup.zsh
@@ -5,8 +5,8 @@ function fixup () {
       # When the first argument is a valid rev use it
       git rev-parse -q --verify "$1"
       if git rev-parse -q --verify "$1" > /dev/null; then
-         commit="$1"
-         shift 1 # the remaining arguments must be appended to the git command
+        commit="$1"
+        shift 1 # the remaining arguments must be appended to the git command
       fi
     fi
     git commit -q --fixup=$commit "$@"
