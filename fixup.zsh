6f9cba863af972da7cbbdb8dfb6b14e80f63145b
Martijn Gastkemper <contact@martijngastkemper.nl>
2026-07-18 04:48:07 +0200

    Fix errors reported by zsh-lint

--- fixup.zsh
+++ fixup.zsh
@@ -1,6 +1,6 @@
 
-function fixup () {
-    commit=`git rev-parse HEAD`
+function fixup {
+    commit=$(git rev-parse HEAD)
     if (( $# >= 1 )); then
       # When the first argument is a valid rev use it
       git rev-parse -q --verify "$1"
@@ -9,6 +9,6 @@ function fixup () {
         shift 1 # the remaining arguments must be appended to the git command
       fi
     fi
-    git commit -q --fixup=$commit "$@"
+    git commit -q --fixup="$commit" "$@"
 }
 
